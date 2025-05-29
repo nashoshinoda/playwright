a374d0670523d1486f2de3cbba2ddef7a166765e
Ignacio <ignacio.velderrain@commscope.com>
2025-05-29 15:15:33 -0700

    Add API files.

---
+++ utils/APiUtils.js
@@ -0,0 +1,45 @@
+class APiUtils {
+  constructor(apiContext, loginPayLoad) {
+    this.apiContext = apiContext;
+    this.loginPayLoad = loginPayLoad;
+    this.baseUrl = "https://rahulshettyacademy.com/api/ecom";
+  }
+
+  async getToken() {
+    const loginResponse = await this.apiContext.post(
+      this.baseUrl + "/auth/login",
+      {
+        data: this.loginPayLoad,
+      }
+    ); // 200, 201
+    const loginResponseJson = await loginResponse.json();
+    const token = loginResponseJson.token;
+    console.log(token);
+
+    return token;
+  }
+
+  async createOrder(orderPayLoad) {
+    let response = {};
+    response.token = await this.getToken();
+    const orderResponse = await this.apiContext.post(
+      this.baseUrl + "/order/create-order",
+      {
+        data: orderPayLoad,
+        headers: {
+          Authorization: response.token,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const orderResponseJson = await orderResponse.json();
+    console.log(orderResponseJson);
+    const orderId = orderResponseJson.orders[0];
+    response.orderId = orderId;
+
+    return response;
+  }
+}
+
+module.exports = { APiUtils };
